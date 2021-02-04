e669fe8561061a8f72056d1ee789f18551bea9e6
Dcdemon <1427907128@qq.com>
2021-02-04 18:38:20 +0800

    两种转发方式实现

--- src/main/java/com/dcdemon/controller/HelloController.java
+++ src/main/java/com/dcdemon/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.dcdemon.controller;
 
+import com.dcdemon.dto.User;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import java.util.Date;
 
 @Controller
-@RequestMapping("/hello")
+@RequestMapping("hello")
 public class HelloController {
 
     @RequestMapping("/test1")
@@ -22,8 +23,43 @@ public class HelloController {
     }
 
     @RequestMapping("/test2")
-    public String test2() {
-        System.out.println("test222");
-        return "success";
+    public String test2(User user) {
+        System.out.println("哈哈");
+        return "view/hello";
     }
+
+    /**
+     * 重定向跳转测试
+     *
+     * @param user
+     * @return
+     */
+    @RequestMapping("/redirectTest")
+    public String test3(User user) {
+        //绝对路径
+        //return "redirect:/hello/test2";
+        //相对路径
+        return "redirect:test2";
+    }
+
+    /**
+     * 转发跳转测试
+     *
+     * @param user
+     * @return
+     */
+    @RequestMapping("/forwardTest")
+    public String test4(User user) {
+        return "forward:test2";
+    }
+
+    /**
+     * @return
+     */
+    @RequestMapping("/registUser")
+    public String registUser() {
+        System.out.println("注册用户");
+        return "redirect:/index.jsp";
+    }
+
 }
---
+++ src/main/java/com/dcdemon/dto/User.java
@@ -0,0 +1,48 @@
+package com.dcdemon.dto;
+
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class User {
+
+    private Integer id;
+
+    private String name;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private Date date;
+
+    private List<String> hobby;
+
+    @JsonInclude(Include.NON_EMPTY)
+    private Map<String, Object> _innerMap;
+
+    @JsonAnySetter
+    public void set_innerMap(String key, Object value) {
+        if (this._innerMap == null) {
+            this._innerMap = new HashMap();
+        }
+        this._innerMap.put(key, value);
+    }
+
+    @JsonAnyGetter
+    public Map<String, Object> get_innerMap() {
+        return this._innerMap;
+    }
+
+}
